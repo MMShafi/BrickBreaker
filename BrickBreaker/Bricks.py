5f403cca69c8fe9c51c7aaae788591c8d0619436
m.shafi1 <m.shafi1@share.epsb.ca>
2020-05-30 20:51:24 -0600

    got bricks scattered across screen

--- BrickBreaker/Bricks.py
+++ BrickBreaker/Bricks.py
@@ -3,13 +3,31 @@ Title: Bricks class
 """
 
 from Sprites import Sprite
+from Window import Window
 
-class Bricks(Sprite):
+class Box(Sprite):
 
     def __init__(self, window):
         Sprite.__init__(self, window)
-        self.setDimensions(125, 45)
+        self.setDimensions(100, 30)
         self.setPOS(self.window.getWidth()/2 - self.width/2, self.window.getHeight()/2 - self.height)
+
+
+class Bricks:
+
+    def __init__(self):
+        self.window = Window()
+        self.bricks = []
+
+        for i in range(7):
+            self.bricks.append(Box(self.window))
+
+        for i in range(len(self.bricks)):
+            self.bricks[i].setPOS((i+0.2) * (self.bricks[i].getWidth() + 10), 15 )
+
+    def blitBricks(self):
+        for brick in self.bricks:
+            self.window.blitSprite(brick)
 
 
 if __name__ == "__main__":
@@ -22,7 +40,7 @@ if __name__ == "__main__":
     window = Window()
     ball = Ball(window)
     paddle = Paddle(window)
-    brick = Bricks(window)
+    bricks = Bricks()
 
     while True:
         window.getEvents()
@@ -33,7 +51,8 @@ if __name__ == "__main__":
         window.clearScreen()
         window.blitSprite(ball)
         window.blitSprite(paddle)
-        window.blitSprite(brick)
+        bricks.blitBricks()
+
         window.updateScreen()
 
 
